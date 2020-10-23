0c67d5a75bcd53b197f06063f4853001ad62a13b
barbosa <omar.barbosa89@outlook.com>
2020-10-23 18:03:14 -0500

    Disable schedule commands temporary

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('backup:seeder')->daily();
+        // $schedule->command('backup:seeder')->daily();
     }
 
     /**
