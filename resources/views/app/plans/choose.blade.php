35f330186eca1af3344c34df57b35da507d43045
barbosa <omar.barbosa89@outlook.com>
2020-11-12 08:05:48 -0500

    Plan cards with margin bottom

--- resources/views/app/plans/choose.blade.php
+++ resources/views/app/plans/choose.blade.php
@@ -15,7 +15,7 @@
 
         <div class="row">
             @foreach ($plans as $plan)
-                <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 mb-4">
                     @include('app.plans.types.cards.' . $plan->getType())
                 </div>
             @endforeach
