7bdedffd08ce09228fe6585be3e987ec5077eb3c
barbosa <omar.barbosa89@outlook.com>
2020-11-12 07:03:11 -0500

    Fix: Navbar menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,11 +29,6 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
@@ -74,5 +69,6 @@
             @yield('content')
         </main>
     </div>
+    <script src="{{ asset('js/theme.js') }}"></script>
 </body>
 </html>
