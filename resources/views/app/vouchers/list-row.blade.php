50f6873797e32152ae060e124e7959d01dc48d23
barbosa <omar.barbosa89@outlook.com>
2021-01-20 00:54:35 -0500

    Fix: Voucher number overflow

--- resources/views/app/vouchers/list-row.blade.php
+++ resources/views/app/vouchers/list-row.blade.php
@@ -1,6 +1,6 @@
 <div class="crud-list-row">
     <div class="row">
-        <div class="col-xs-6 col-sm-6 col-md-1 col-lg-1 align-self-center">
+        <div class="col-xs-6 col-sm-6 col-md-1 col-lg-1 align-self-center dont-break-out">
             <p>
                 <a href="{{ route('vouchers.show', ['id' => id_encode($row->id)]) }}">
                     {{ $row->number }}
