9dca895e56f6d54cb2eaa517546d299f08c6692e
barbosa <omar.barbosa89@outlook.com>
2021-01-25 14:54:06 -0500

    Add Bootstrap 3 support

--- resources/views/app/vouchers/exports/header.blade.php
+++ resources/views/app/vouchers/exports/header.blade.php
@@ -1,7 +1,7 @@
 <!-- header -->
 <div class="header">
     <div class="row">
-        <div class="col-6 from">
+        <div class="col-xs-6 from">
             <span class="d-block font-weight-light">@lang('vouchers.from'):</span>
             @if ($voucher->type == 'entry')
                 @include('app.vouchers.exports.customer')
@@ -9,7 +9,7 @@
                 @include('app.vouchers.exports.hotel')
             @endif
         </div>
-        <div class="col-6 to">
+        <div class="col-xs-6 to">
             <span class="d-block font-weight-light">@lang('vouchers.to'):</span>
             @if ($voucher->type == 'entry')
                 @include('app.vouchers.exports.hotel')
@@ -23,7 +23,7 @@
 
 <!-- note -->
 <div class="row mt-4">
-    <div class="col-12">
+    <div class="col-xs-12">
         <p class="text-justify text-muted">
             <small>{{ $voucher->comments }}</small>
         </p>
--- resources/views/app/vouchers/exports/template.blade.php
+++ resources/views/app/vouchers/exports/template.blade.php
@@ -2,15 +2,81 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>@lang('common.invoice')</title>
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="stylesheet" href="{{ public_path('vendor/invoices/bootstrap.min.css') }}">
+
     <!-- Styles -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
     <style>
+        h1, .h1, h2, .h2, h3, .h3 {
+            margin-top: 10px !important;
+        }
+        .no-padding {
+            padding-right: 0 !important;
+            padding-left: 0 !important;
+        }
+
+        .no-padding-left {
+            padding-left: 0 !important;
+        }
+
+        .img-fluid {
+            max-width: 100%;
+            height: auto;
+        }
+        .mt-2 {
+            margin-top: 10px;
+        }
+        .mt-4 {
+            margin-top: 20px;
+        }
+
+        .mb-2 {
+            margin-bottom: 20px;
+        }
+        .mb-4 {
+            margin-bottom: 20px;
+        }
+
+        .my-2 {
+            margin: 0 10px;
+        }
+
+        .my-4 {
+            margin: 0 20px;
+        }
+
+        .mx-2 {
+            margin: 10px 0;
+        }
+
+        .mx-4 {
+            margin: 20px 0;
+        }
+
+        .py-2 {
+            padding: 0 10px;
+        }
+
+        .py-4 {
+            padding: 0 20px;
+        }
+
+        .d-block {
+            display: block;
+        }
+
+        .font-weight-light {
+            font-weight: 100;
+        }
+
+        .font-weight-bold {
+            font-weight: bold;
+        }
+
         .line {
             border-bottom: 2px solid #000;
             width: 100%;
@@ -51,6 +117,22 @@
             margin: 80px 0 20px 0;
         }
 
+        .without-margin {
+            margin: 0 !important;
+        }
+
+        .image {
+            margin: 0 auto;
+            display: block;
+        }
+
+        /* To break in pages, please use this class */
+        .page
+        {
+            page-break-after: always;
+            page-break-inside: avoid;
+        }
+
         .row-equal {
             display: table;
         }
@@ -60,6 +142,179 @@
             display: table-cell;
             vertical-align: top;
         }
+
+        .spacer-lg {
+            margin: 20px;
+            display: block;
+        }
+
+        .table {
+            width: 100%;
+            max-width: 100%;
+            margin-bottom: 1rem;
+            }
+
+            .table th,
+            .table td {
+            padding: 0.75rem;
+            vertical-align: top;
+            border-top: 1px solid #eceeef;
+            }
+
+            .table thead th {
+            vertical-align: bottom;
+            border-bottom: 2px solid #eceeef;
+            }
+
+            .table tbody + tbody {
+            border-top: 2px solid #eceeef;
+            }
+
+            .table .table {
+            background-color: #fff;
+            }
+
+            .table-sm th,
+            .table-sm td {
+            padding: 0.3rem;
+            }
+
+            .table-bordered {
+            border: 1px solid #eceeef;
+            }
+
+            .table-bordered th,
+            .table-bordered td {
+            border: 1px solid #eceeef;
+            }
+
+            .table-bordered thead th,
+            .table-bordered thead td {
+            border-bottom-width: 2px;
+            }
+
+            .table-striped tbody tr:nth-of-type(odd) {
+            background-color: rgba(0, 0, 0, 0.05);
+            }
+
+            .table-hover tbody tr:hover {
+            background-color: rgba(0, 0, 0, 0.075);
+            }
+
+            .table-active,
+            .table-active > th,
+            .table-active > td {
+            background-color: rgba(0, 0, 0, 0.075);
+            }
+
+            .table-hover .table-active:hover {
+            background-color: rgba(0, 0, 0, 0.075);
+            }
+
+            .table-hover .table-active:hover > td,
+            .table-hover .table-active:hover > th {
+            background-color: rgba(0, 0, 0, 0.075);
+            }
+
+            .table-success,
+            .table-success > th,
+            .table-success > td {
+            background-color: #dff0d8;
+            }
+
+            .table-hover .table-success:hover {
+            background-color: #d0e9c6;
+            }
+
+            .table-hover .table-success:hover > td,
+            .table-hover .table-success:hover > th {
+            background-color: #d0e9c6;
+            }
+
+            .table-info,
+            .table-info > th,
+            .table-info > td {
+            background-color: #d9edf7;
+            }
+
+            .table-hover .table-info:hover {
+            background-color: #c4e3f3;
+            }
+
+            .table-hover .table-info:hover > td,
+            .table-hover .table-info:hover > th {
+            background-color: #c4e3f3;
+            }
+
+            .table-warning,
+            .table-warning > th,
+            .table-warning > td {
+            background-color: #fcf8e3;
+            }
+
+            .table-hover .table-warning:hover {
+            background-color: #faf2cc;
+            }
+
+            .table-hover .table-warning:hover > td,
+            .table-hover .table-warning:hover > th {
+            background-color: #faf2cc;
+            }
+
+            .table-danger,
+            .table-danger > th,
+            .table-danger > td {
+            background-color: #f2dede;
+            }
+
+            .table-hover .table-danger:hover {
+            background-color: #ebcccc;
+            }
+
+            .table-hover .table-danger:hover > td,
+            .table-hover .table-danger:hover > th {
+            background-color: #ebcccc;
+            }
+
+            .thead-inverse th {
+            color: #fff;
+            background-color: #292b2c;
+            }
+
+            .thead-default th {
+            color: #464a4c;
+            background-color: #eceeef;
+            }
+
+            .table-inverse {
+            color: #fff;
+            background-color: #292b2c;
+            }
+
+            .table-inverse th,
+            .table-inverse td,
+            .table-inverse thead th {
+            border-color: #fff;
+            }
+
+            .table-inverse.table-bordered {
+            border: 0;
+            }
+
+            .table-responsive {
+            display: block;
+            width: 100%;
+            overflow-x: auto;
+            -ms-overflow-style: -ms-autohiding-scrollbar;
+            }
+
+            .table-responsive.table-bordered {
+            border: 0;
+            }
+
+            body {
+                font-family: 'Courier New', Courier, monospace;
+            }
     </style>
 </head>
 <body>
@@ -67,13 +322,13 @@
         <div id="app" class="container voucher page">
             <div class="container">
                 <div class="row mt-4">
-                    <div class="col-3">
+                    <div class="col-xs-3">
                         <!-- data -->
                         @include('app.vouchers.exports.data')
                         <!-- end data -->
                     </div>
 
-                    <div class="col-9 mt-4">
+                    <div class="col-xs-9 mt-4">
                         <!-- content -->
                         <div class="content">
                             <div class="line mb-4"></div>
