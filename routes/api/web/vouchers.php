11017ac3f808d8b0f3294da3264720ed70ef1f69
barbosa <contacto@omarbarbosa.com>
2021-03-17 23:14:58 -0500

    Chore: Route to get guest chartdata

--- routes/api/web/vouchers.php
+++ routes/api/web/vouchers.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'api/v1/web', 'as' => 'api.web.', 'middleware' => ['auth', 'verified']], function() {
+	Route::get('hotels/{hotel}/vouchers/datasets/guests/{period}', 'Api\VoucherController@getGuestDataset')
+		->name('vouchers.datasets.guests')
+		->middleware('permission:vouchers.index');
+
     Route::get('hotels/{hotel}/vouchers', 'Api\VoucherController@index')
         ->name('vouchers.index')
         ->middleware(['permission:vouchers.index']);
