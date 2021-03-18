ff27f6794abf20e41965e9d65582a12c7e9fda72
barbosa <contacto@omarbarbosa.com>
2021-03-17 23:11:50 -0500

    Refactor: Remove non required code

--- app/Http/Controllers/Api/RoomController.php
+++ app/Http/Controllers/Api/RoomController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
 use App\Http\Requests\StoreRoom;
 use App\Contracts\RoomRepository;
 use App\Http\Controllers\Controller;
@@ -60,40 +59,6 @@ class RoomController extends Controller
         return response()->json([
             'room' => $room,
         ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 
     /**
