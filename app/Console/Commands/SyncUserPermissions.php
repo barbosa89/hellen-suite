d6ec322ce61b1ee6b9ecea9d77fa2cd655d2ab87
barbosa89 <contacto@omarbarbosa.com>
2021-07-07 17:49:21 -0500

    Feat: Command to sync manager user permissions

---
+++ app/Console/Commands/SyncUserPermissions.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\LazyCollection;
+use Spatie\Permission\Models\Permission;
+
+class SyncUserPermissions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'permissions:sync';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sync manager user permissions';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        DB::transaction(function () {
+            $permissions = Permission::all(['id', 'name', 'guard_name']);
+
+            LazyCollection::make(function () {
+                return User::owner()->get(['id']);
+            })->each(function (User $user) use ($permissions) {
+                $user->syncPermissions($permissions);
+            });
+        });
+
+        return 0;
+    }
+}
