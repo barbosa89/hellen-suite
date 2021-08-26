cf37136c8133c3d78d222c7140578a1e57c3cb06
Barbosa89 <contacto@omarbarbosa.com>
2021-08-26 06:51:21 -0500

    Refactor: Use navigation component in Voucher index

--- resources/js/components/Vouchers/Index.vue
+++ resources/js/components/Vouchers/Index.vue
@@ -1,30 +1,5 @@
 <template>
     <div>
-        <nav class="navbar navbar-expand-lg navbar-light app-nav border border-top-0 border-right-0 border-left-0">
-            <a href="/vouchers" class="navbar-brand text-muted">
-                {{ $t('vouchers.title') }}
-            </a>
-            <button type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div id="navbarNavDropdown" class="collapse navbar-collapse">
-                <form class="form-inline my-2 my-lg-0">
-                    <hotel-select @hotel="hotelHash = $event"></hotel-select>
-                </form>
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a :href='route("vouchers.process")' rel="noopener noreferrer" class="nav-link">
-                            {{ $t('vouchers.process') }}
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" data-toggle="modal" data-target="#voucher-filter" class="nav-link">
-                            {{ $t('common.filters.filters') }}
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
         <div class="row mt-3" v-if="canDisplayList">
             <div class="col">
                 <vue-table
@@ -53,77 +28,81 @@
 </template>
 
 <script>
-    import VueTable from '@barbosa89/vue-table'
-    import FilterModal from './FilterModal'
+import FilterModal from './FilterModal'
+import VueTable from '@barbosa89/vue-table'
 
-    export default {
-        mounted() {
-            this.lang = document.documentElement.lang
-        },
-        components: {
-            VueTable,
-            FilterModal
-        },
-        computed: {
-            canDisplayList() {
-                return this.url.length > 0
-            }
-        },
-        data() {
-            return {
-                url: '',
-                hotelHash: '',
-                headers: [
-                    {
-                        description: this.$root.$t('common.date')
-                    },
-                    {
-                        description: this.$root.$t('common.number'),
-                        sortable: 'number'
-                    },
-                    {
-                        description: this.$root.$t('common.type'),
-                        sortable: 'type'
-                    },
-                    {
-                        description: this.$root.$t('common.value')
-                    },
-                ],
-                lang: '',
-                params: {}
-            }
-        },
-        watch: {
-            hotelHash() {
-                if (this.hotelHash.length) {
-                    this.url = ''
+export default {
+    mounted() {
+        this.lang = document.documentElement.lang
 
-                    this.url = route('api.web.vouchers.index', this.hotelHash)
-                }
-            }
-        },
-        methods: {
-            close() {
-                $('#voucher-filter').modal('hide')
-            },
-            toggle() {
-                $('#voucher-filter').modal('toggle')
-            },
-            setFilters(filters) {
-                const params = {}
+        this.$root.$on('hotel-selected', (hash) => {
+            this.hotelHash = hash
+        })
+    },
+    components: {
+        VueTable,
+        FilterModal
+    },
+    computed: {
+        canDisplayList() {
+            return this.url.length > 0
+        }
+    },
+    data() {
+        return {
+            url: '',
+            hotelHash: '',
+            headers: [
+                {
+                    description: this.$root.$t('common.date')
+                },
+                {
+                    description: this.$root.$t('common.number'),
+                    sortable: 'number'
+                },
+                {
+                    description: this.$root.$t('common.type'),
+                    sortable: 'type'
+                },
+                {
+                    description: this.$root.$t('common.value')
+                },
+            ],
+            lang: '',
+            params: {}
+        }
+    },
+    watch: {
+        hotelHash() {
+            if (this.hotelHash.length) {
+                this.url = ''
 
-                if (filters.hasOwnProperty('status') && filters.status.length) {
-                    params.status = filters.status
-                }
-
-                if (filters.hasOwnProperty('type') && filters.type.length) {
-                    params.type = filters.type
-                }
-
-                this.params = Object.assign({}, params)
-
-                this.close()
+                this.url = route('api.web.vouchers.index', this.hotelHash)
             }
         }
+    },
+    methods: {
+        close() {
+            $('#voucher-filter').modal('hide')
+        },
+        toggle() {
+            $('#voucher-filter').modal('toggle')
+        },
+        setFilters(filters) {
+            const params = {}
+
+            if (filters.hasOwnProperty('status') && filters.status.length) {
+                params.status = filters.status
+            }
+
+            if (filters.hasOwnProperty('type') && filters.type.length) {
+                params.type = filters.type
+            }
+
+            this.params = Object.assign({}, params)
+
+            this.close()
+        }
     }
+}
 </script>
--- resources/views/app/vouchers/index.blade.php
+++ resources/views/app/vouchers/index.blade.php
@@ -5,6 +5,29 @@
 @endsection
 
 @section('content')
+    <x-navigation
+        title="{{ trans('vouchers.title') }}"
+        url="{{ route('vouchers.index') }}"
+        hotel-select>
+        <ul class="navbar-nav ml-auto">
+            @can('vouchers.edit')
+                <li class="nav-item">
+                    <a href="{{ route('vouchers.process') }}" class="nav-link">
+                        {{ trans('vouchers.process') }}
+                    </a>
+                </li>
+            @endcan
+
+            @can('vouchers.index')
+                <li class="nav-item">
+                    <a href="#" data-toggle="modal" data-target="#voucher-filter" class="nav-link">
+                        {{ trans('common.filters.filters') }}
+                    </a>
+                </li>
+            @endcan
+        </ul>
+    </x-navigation>
+
     <vouchers-index></vouchers-index>
 
     @include('partials.modal-confirm')
