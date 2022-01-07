40b1d61c9bdfceb389fed2ca9f9df2cf1d9f673d
Barbosa <omar.barbosa@evertecinc.com>
2022-01-07 17:21:41 -0500

    Style: Format

--- resources/lang/en/rooms.php
+++ resources/lang/en/rooms.php
@@ -1,39 +1,36 @@
 <?php
 
 return array (
-  'title' => 'Rooms',
-  'disabled' => 'Disabled',
-  'free' => 'Free',
-  'maintenance' => 'Maintenance',
-  'occupied' => 'Occupied',
-  'successful' => 'The room was created successfully',
-  'room' => 'Room',
-  'wasDisabled' => 'The room was disabled',
-  'intoMaintenance' => 'The room went into maintenance',
-  'reserve' => 'Reserve',
-  'addRoom' => 'Add room',
-  'chooseRoom' => 'Choose a room',
-  'available' => 'Available',
-  'cleaning' => 'Cleaning',
-  'availableRooms' => 'Available rooms',
-  'changeRoom' => 'Change room',
-  'currentRoom' => 'Current room',
-  'deliver' => 'Deliver the room',
-  'no' => 
-  array (
-    'created' => 'There are no rooms created',
-  ),
-  'type' => 'Indicates if the room is a suite',
-  'capacity' => 'Number of people to stay in the room',
-  'cannot' => 
-  array (
-    'enable' => 'You cannot enable this room',
-    'add' => 'You cannot add this room',
-    'destroy' => 'It is not possible to delete the room if it has associated vouchers, you can disable it.',
-  ),
-  'number' => 'Room No. :number',
-  'change' => 
-  array (
-    'disabled' => 'The room cannot be changed',
-  ),
+    'title' => 'Rooms',
+    'disabled' => 'Disabled',
+    'free' => 'Free',
+    'maintenance' => 'Maintenance',
+    'occupied' => 'Occupied',
+    'successful' => 'The room was created successfully',
+    'room' => 'Room',
+    'wasDisabled' => 'The room was disabled',
+    'intoMaintenance' => 'The room went into maintenance',
+    'reserve' => 'Reserve',
+    'addRoom' => 'Add room',
+    'chooseRoom' => 'Choose a room',
+    'available' => 'Available',
+    'cleaning' => 'Cleaning',
+    'availableRooms' => 'Available rooms',
+    'changeRoom' => 'Change room',
+    'currentRoom' => 'Current room',
+    'deliver' => 'Deliver the room',
+    'no' =>  [
+        'created' => 'There are no rooms created',
+    ],
+    'type' => 'Indicates if the room is a suite',
+    'capacity' => 'Number of people to stay in the room',
+    'cannot' => [
+        'enable' => 'You cannot enable this room',
+        'add' => 'You cannot add this room',
+        'destroy' => 'It is not possible to delete the room if it has associated vouchers, you can disable it.',
+    ],
+    'number' => 'Room No. :number',
+    'change' => [
+        'disabled' => 'The room cannot be changed',
+    ],
 );
--- resources/lang/es/rooms.php
+++ resources/lang/es/rooms.php
@@ -1,39 +1,36 @@
 <?php
 
 return array (
-  'title' => 'Habitaciones',
-  'occupied' => 'Ocupado',
-  'free' => 'Libre',
-  'maintenance' => 'Mantenimiento',
-  'disabled' => 'Deshabilitado',
-  'successful' => 'La habitación fue creada satisfactoriamente',
-  'room' => 'Habitación',
-  'wasDisabled' => 'La habitación fue deshabilitada',
-  'intoMaintenance' => 'La habitación entro a mantenimiento',
-  'reserve' => 'Reservar',
-  'addRoom' => 'Agregar habitación',
-  'chooseRoom' => 'Elije una habitación',
-  'available' => 'Disponible',
-  'cleaning' => 'Limpieza',
-  'changeRoom' => 'Cambiar de habitación',
-  'currentRoom' => 'Habitación actual',
-  'availableRooms' => 'Habitaciones disponibles',
-  'deliver' => 'Entregar la habitación',
-  'no' => 
-  array (
-    'created' => 'No hay habitaciones creadas',
-  ),
-  'type' => 'Indica si la habitación es una suite',
-  'capacity' => 'Cantidad de personas a hospedar en la habitación',
-  'cannot' => 
-  array (
-    'enable' => 'Tú no puedes habilitar esta habitación',
-    'add' => 'Tú no puedes agregar esta habitación',
-    'destroy' => 'No es posible eliminar la habitación si tiene comprobantes asociados, puede deshabilitarla.',
-  ),
-  'number' => 'Habitación No. :number',
-  'change' => 
-  array (
-    'disabled' => 'La habitación no puede ser cambiada',
-  ),
+    'title' => 'Habitaciones',
+    'occupied' => 'Ocupado',
+    'free' => 'Libre',
+    'maintenance' => 'Mantenimiento',
+    'disabled' => 'Deshabilitado',
+    'successful' => 'La habitación fue creada satisfactoriamente',
+    'room' => 'Habitación',
+    'wasDisabled' => 'La habitación fue deshabilitada',
+    'intoMaintenance' => 'La habitación entro a mantenimiento',
+    'reserve' => 'Reservar',
+    'addRoom' => 'Agregar habitación',
+    'chooseRoom' => 'Elije una habitación',
+    'available' => 'Disponible',
+    'cleaning' => 'Limpieza',
+    'changeRoom' => 'Cambiar de habitación',
+    'currentRoom' => 'Habitación actual',
+    'availableRooms' => 'Habitaciones disponibles',
+    'deliver' => 'Entregar la habitación',
+    'no' => [
+        'created' => 'No hay habitaciones creadas',
+    ],
+    'type' => 'Indica si la habitación es una suite',
+    'capacity' => 'Cantidad de personas a hospedar en la habitación',
+    'cannot' => [
+        'enable' => 'Tú no puedes habilitar esta habitación',
+        'add' => 'Tú no puedes agregar esta habitación',
+        'destroy' => 'No es posible eliminar la habitación si tiene comprobantes asociados, puede deshabilitarla.',
+    ],
+    'number' => 'Habitación No. :number',
+    'change' => [
+        'disabled' => 'La habitación no puede ser cambiada',
+    ],
 );
