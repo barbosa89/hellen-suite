3094aa120e9980153f26933f97d41f91ad358230
Barbosa <omar.barbosa@evertecinc.com>
2022-03-21 16:08:30 -0500

    Refactor: Update translations

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -1,14 +1,15 @@
 <?php
 
-return array (
-  'failed' => 'Estas credenciales no coinciden con nuestros registros.',
-  'throttle' => 'Demasiados intentos de inicio de sesión. Vuelva a intentarlo en :seconds segundos.',
-  'verify' => 
-  array (
-    'title' => 'Verifica tu dirección de correo electrónico',
-    'reset' => 'Un nuevo enlace de verificación ha sido enviado a tu dirección de correo electrónico.',
-    'link' => 'Antes de continuar, por favor verifica en tu correo electrónico un mensaje con un enlace de verificación.',
-    'receive' => 'Si no recibiste el correo electrónico',
-    'click' => 'clic aquí para enviar otro.',
-  ),
+return array(
+    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña provista no es correcta.',
+    'throttle' => 'Demasiados intentos de inicio de sesión. Vuelva a intentarlo en :seconds segundos.',
+    'verify' =>
+    array(
+        'title' => 'Verifica tu dirección de correo electrónico',
+        'reset' => 'Un nuevo enlace de verificación ha sido enviado a tu dirección de correo electrónico.',
+        'link' => 'Antes de continuar, por favor verifica en tu correo electrónico un mensaje con un enlace de verificación.',
+        'receive' => 'Si no recibiste el correo electrónico',
+        'click' => 'clic aquí para enviar otro.',
+    ),
 );
