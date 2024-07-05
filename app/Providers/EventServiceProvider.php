a053008c9c6331eaa00742c48970bb4f37746f7f
Omar Barbosa <omar.barbosa@evertecinc.com>
2024-07-05 14:38:10 -0500

    feat: Laravel 9 support

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvi
 class EventServiceProvider extends ServiceProvider
 {
     /**
-     * The event listener mappings for the application.
+     * The event to listener mappings for the application.
      *
      * @var array<class-string, array<int, class-string>>
      */
@@ -36,5 +36,13 @@ class EventServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+    }
+
+    /**
+     * Determine if events and listeners should be automatically discovered.
+     */
+    public function shouldDiscoverEvents(): bool
+    {
+        return false;
     }
 }
