774ac4cc9d0d601b97c5bc2bd2f0e6e2a8076cd7
luisJaimeB <luisyi1998@gmail.com>
2024-11-11 16:42:27 -0500

    chore: update App\Providers\AuthServiceProvider for Laravel 10 compatibility.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -12,7 +12,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        //
     ];
 
     /**
@@ -20,8 +20,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->registerPolicies();
-
         //
     }
 }
